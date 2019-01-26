92187da5a7304a820dd3fbca0e02e9ce96f9e08a
FloNeu <di.neumann.florian@gmail.com>
2019-01-26 11:24:28 +0100

    Added component for data-table based on angular-material cdk

---
+++ frontend/pb-pwa/src/app/component/mat-table/mat-table.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'pb-mat-table',
+  templateUrl: './mat-table.component.html',
+  styleUrls: ['./mat-table.component.css']
+})
+export class MatTableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
