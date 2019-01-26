1e4bb8627e0055083d63a07847436992ad03c932
FloNeu <di.neumann.florian@gmail.com>
2019-01-26 17:42:48 +0100

    Add component placeholder for signaling and handling offline-state

---
+++ frontend/pb-pwa/src/app/component/pwa-offline/pwa-offline.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'pb-pwa-offline',
+  templateUrl: './pwa-offline.component.html',
+  styleUrls: ['./pwa-offline.component.scss']
+})
+export class PwaOfflineComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
